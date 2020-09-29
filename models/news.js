5e9e4933f530397f18e7efeebfa12ad087a62a57
unknown <jorcuecos@gmail.com>
2020-09-29 18:09:11 +0200

    feat: filtering and sorting

--- models/news.js
+++ models/news.js
@@ -1,39 +1,51 @@
-var mongoose = require('mongoose');
-var mongoosePaginate = require('mongoose-paginate-v2');
+var mongoose = require("mongoose");
+var mongoosePaginate = require("mongoose-paginate-v2");
+const { parseSort } = require("../utils/utils");
 
 var NewsSchema = new mongoose.Schema(
-    {
-        title: String,
-        description: String,
-        content: String,
-        author: String,
-        archiveDate: Date
-    },
-    {
-        timestamps: true,
-        strict: true
-    });
+  {
+    title: String,
+    description: String,
+    content: String,
+    author: String,
+    archiveDate: Date,
+  },
+  {
+    timestamps: true,
+    strict: true,
+  }
+);
 NewsSchema.plugin(mongoosePaginate);
 
-var newsModel = mongoose.model('New', NewsSchema);
+var newsModel = mongoose.model("New", NewsSchema);
 
 exports.findById = function (id) {
-    return newsModel.findById(id)
-}
+  return newsModel.findById(id);
+};
 
 exports.findMany = function (query) {
-    return newsModel.paginate(query)
-}
+  let sort = query.sort;
+  delete query.sort;
+  if (!sort) {
+    sort = { createdAt: -1 };
+  } else {
+    sort = parseSort(sort);
+  }
+  if (query.archiveDate) {
+    query.archiveDate = { $exists: query.archiveDate == "true" };
+  }
+  return newsModel.paginate(query, { sort: sort });
+};
 
 exports.createOne = function (body) {
-    var model = new newsModel(body);
-    return model.save();
-}
+  var model = new newsModel(body);
+  return model.save();
+};
 
 exports.updateOne = function (id, body) {
-    return newsModel.updateOne({ _id: id }, body)
-}
+  return newsModel.updateOne({ _id: id }, body);
+};
 
 exports.deleteById = function (id) {
-    return newsModel.deleteOne({ _id: id })
-}
+  return newsModel.deleteOne({ _id: id });
+};
